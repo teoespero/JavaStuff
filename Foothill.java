9e8a18ab2696542816bb3b8e12495d7799279560
Teo Espero <38012835+teoespero@users.noreply.github.com>
2020-06-29 23:54:12 -0700

    Add files via upload

---
+++ Foothill.java
@@ -0,0 +1,65 @@
+public class Foothill
+{
+   public static void main(String[] args) 
+   {
+      System.out.println("Hello Foothill Java Kids!");
+      System.out.println("My name is Teodulfo Espero");
+      System.out.println("My student ID is 20314854");
+      System.out.println();   
+      System.out.println("CS 1A, Section 10114 online");   
+      System.out.println(); 
+      System.out.println("Syllabus");
+      System.out.println("--------"); 
+      System.out.println(); 
+      System.out.println("Description: \n" + 
+    		"CS 1A is an introduction to computer programming using the Java language. You\n" + 
+      		"will be learning how to write Java programs that cover a wide range of applications. For\n" + 
+      		"success, however, you will also need both a desire to learn and a positive attitude.");
+      System.out.println(); 
+      System.out.println("Lab Homework Requirements");
+      System.out.println("-------------------------");
+      System.out.println(); 
+      System.out.println("Not Running. You will not get any points for a program that doesn't at least\n" + 
+    		"compile and run. If you can't get the program to run, keep trying to debug it, ask for\n" + 
+    		"help, and hand it in late.  On-time non-running = zero.  Late, working = many points.\n" + 
+    		"The correct choice should be obvious."); 
+      System.out.println(); 
+      System.out.println("Doesn't Run to Spec. You will lose many points (from 3 to 10) for a program\n" + 
+    		"that doesn't run to spec. Again, if you know it's not meeting the required functionality in\n" + 
+    		"the spec, ask for help. But consider the level of your program's deficit. If it meets 98% of\n" + 
+    		"the functional requirements, you will probably only get -2 or -3 for the missing aspects. So\n" + 
+    		"you might not want to spend 3 late day fixing that, since 3 days late is -6 points. Use judgement."); 
+   }
+}
+
+/* ---------------- sample run --------------------------------
+
+Hello Foothill Java Kids!
+My name is Teodulfo Espero
+My student ID is 20314854
+
+CS 1A, Section 10114 online
+
+Syllabus
+--------
+
+Description: 
+CS 1A is an introduction to computer programming using the Java language. You
+will be learning how to write Java programs that cover a wide range of applications. For
+success, however, you will also need both a desire to learn and a positive attitude.
+
+Lab Homework Requirements
+-------------------------
+
+Not Running. You will not get any points for a program that doesn't at least
+compile and run. If you can't get the program to run, keep trying to debug it, ask for
+help, and hand it in late.  On-time non-running = zero.  Late, working = many points.
+The correct choice should be obvious.
+
+Doesn't Run to Spec. You will lose many points (from 3 to 10) for a program
+that doesn't run to spec. Again, if you know it's not meeting the required functionality in
+the spec, ask for help. But consider the level of your program's deficit. If it meets 98% of
+the functional requirements, you will probably only get -2 or -3 for the missing aspects. So
+you might not want to spend 3 late day fixing that, since 3 days late is -6 points. Use judgement.
+
+------------------------------------------------------------- */
